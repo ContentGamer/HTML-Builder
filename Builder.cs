7ae5c72fee7b00d0135a896ca8e89370c0b37545
ContentGamer <85808595+ContentGamer@users.noreply.github.com>
2023-01-03 21:38:26 +0100

    Initial commit

---
+++ Builder.cs
@@ -0,0 +1,17 @@
+﻿namespace HTMLBuilder;
+
+public class Builder : Handler
+{
+    public Builder() {}
+ 
+    public dynamic Build(bool returnValue = false) {
+        string output = "";
+        if(returnValue) {
+            output = GetCode();
+        }else {
+            output = "SET THE 'returnValue' TO TRUE TO RETURN A STRING!!";
+            Console.WriteLine(GetCode());
+        }
+        return output;
+    }
+}
---
+++ Handler.cs
@@ -0,0 +1,8 @@
+namespace HTMLBuilder;
+
+public class Handler {
+    public string GetCode() {
+        return 
+        "<!DOCTYPE html>\n\n<html lang='en'>\n  <head>\n    <meta charset='UTF-8'>\n    <meta http-equiv='X-UA-Compatible' content='IE=edge'>\n    <meta name='viewport' content='width=device-width, initial-scale=1.0'>\n    <title>Document</title>\n  </head>\n\n  <body>\n    <br>TEST</br>\n    <p>This is a paragraph, this HTML Builder has some elements.</p>\n  </body>\n</html>";
+    }
+}
